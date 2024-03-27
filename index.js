25b7cdf14c58763fe99aa99c56a791a4dec243d3
Michael <oliengo.michael@gmail.com>
2024-03-27 18:47:20 +0300

    submit Control Flow Lab

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceCovered) {
+
+  if (distanceCovered <= 400){
+          return "This one is on me!";
+  }
+  else if (distanceCovered > 400 && distanceCovered < 2000){
+    return "That will be twenty bucks.";
+}
+  else if (distanceCovered > 2000 && distanceCovered < 2500){
+      return "I will gladly take your thirty bucks.";
+  }
+  else if(distanceCovered > 2500){
+      return "No can do.";
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destinationCity){
+  if (destinationCity === "NYC"){
+    return "Ok, sounds good.";
+  }
+  else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(Tip){
+ if (Tip === "generous"){
+  return "Thank you so much.";
+ }
+ else if (Tip === "not as generous"){
+  return "Thank you.";
+ }
+ else{
+  return "Bye."
+ }
 }
